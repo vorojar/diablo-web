6bdd67fadf1c308c302f43febbc5835e54132b0a
vorojar <vorojar@gmail.com>
2026-05-01 09:46:04 +0800

    Fix side walk sprite proportions

--- game.js
+++ game.js
@@ -979,7 +979,7 @@ function createTintedSpriteSheet(source, filterStr) {
 }
 // --- Hero Animation Sprites ---
 const heroSpriteSheet = new Image();
-heroSpriteSheet.src = 'hero_sprites.png?v=202605010530';
+heroSpriteSheet.src = 'hero_sprites.png?v=202605010600';
 let heroSpritesLoaded = false;
 let processedHeroSprites = null;
 const HeroTintCache = {
