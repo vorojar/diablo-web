619c25a138dbc216f209cd6a579a028f40eff040
vorojar <vorojar@gmail.com>
2026-05-01 09:09:06 +0800

    Clean hero sprite edges and player changelog

--- game.js
+++ game.js
@@ -979,7 +979,7 @@ function createTintedSpriteSheet(source, filterStr) {
 }
 // --- Hero Animation Sprites ---
 const heroSpriteSheet = new Image();
-heroSpriteSheet.src = 'hero_sprites.png?v=202605010130';
+heroSpriteSheet.src = 'hero_sprites.png?v=202605010400';
 let heroSpritesLoaded = false;
 let processedHeroSprites = null;
 const HeroTintCache = {
